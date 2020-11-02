df7f3fd0a6fb23577f371434a02a9f84d74b1370
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-11-02 16:24:21 -0500

    Fin args

--- args.py
+++ args.py
@@ -4,11 +4,17 @@ Programme pour afficher les arguments
 
 Austin Brodeur
 """
+import sys
+import pprint
 
 
-def main() -> None:
+def main():
     """Fonction principale"""
-    pass
+    pprint.pprint(sys.argv, width=40)
+    print()
+    print(f"Il y a {len(sys.argv) - 1} arguments")
+    for i, arg in enumerate(sys.argv):
+        print(f" - arg {i}: {arg}")
 
 
 if __name__ == '__main__':
