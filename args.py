f5fb384195b9278cc558705220e5b4bf230852a9
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-11-02 15:43:03 -0500

    Initial commit

---
+++ args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Programme pour afficher les arguments
+
+Austin Brodeur
+"""
+
+
+def main() -> None:
+    """Fonction principale"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
