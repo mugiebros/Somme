64a971882cbc1908905e85c12765fa377bcb2568
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-11-17 03:39:13 -0500

    Fin pygrep

---
+++ pygrep.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+"""
+programme pour compter les lignes
+
+austin brodeur
+"""
+
+import re
+import sys
+
+from colorama import Fore
+
+
+def main() -> None:
+    """
+        fonction principale
+    """
+    if sys.stdin.isatty():
+        print("Aucune redirection d'entrée à traiter")
+        return
+    if len(sys.argv) > 1:
+        print(Fore.RED + f"Le code s'attend a recevoir 1 argument, mais vous en avez fourni {len(sys.argv)}",
+              file=sys.stderr)
+        print(Fore.YELLOW + "Usage: ./pygrep.py pattern" + Fore.WHITE, file=sys.stderr)
+        sys.exit(1)
+    compteurCo = 0
+    lignes = sys.stdin.readlines()
+    try:
+        regexp = re.compile(sys.argv[1])
+    except:
+        print(Fore.RED + f"Le code s'attend a recevoir 1 argument, mais vous en avez fourni {len(sys.argv)}",
+              file=sys.stderr)
+        print(Fore.YELLOW + "Usage: ./pygrep.py pattern" + Fore.WHITE, file=sys.stderr)
+        sys.exit(1)
+    for ligne in lignes:
+        if regexp.search(ligne):
+            print(ligne, end=" ")
+            compteurCo += 1
+
+    if compteurCo <= 0:
+        print(Fore.YELLOW + f"[ aucune correspondances ]" + Fore.WHITE, file=sys.stderr)
+    elif compteurCo == 1:
+        print(Fore.YELLOW + f"[ {compteurCo} correspondance ]" + Fore.WHITE, file=sys.stderr)
+    else:
+        print(Fore.YELLOW + f"[ {compteurCo} correspondances ]" + Fore.WHITE, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
--- pytouch.py
+++ pytouch.py
@@ -23,7 +23,7 @@ def parse_arg() -> argparse.Namespace:
     groupe = parser.add_mutually_exclusive_group()
     groupe.add_argument('-q', '--quiet', action='store_true', help='Ne pas spécifier si existe déjà')
     groupe.add_argument('-v', '--verbeux', action=r'store_true', help=r"Notifier le créateur")
-    parser.add_argument('-t', '--texte', type=str, metavar='TXT',default="",
+    parser.add_argument('-t', '--texte', type=str, metavar='TXT', default="",
                         help='Texte a stocker dans les fichiers crées')
 
     args = parser.parse_args()
@@ -42,8 +42,11 @@ def main() -> None:
                 f = open(i, "w+")
                 if parse_arg().verbeux:
                     print(Fore.WHITE + "Fichier Créé:" + Fore.CYAN + f"{i}")
+
                 f.write(parse_arg().texte)
                 f.close()
+
+
     except OSError as ex:
         print(Fore.RED + f"{type(ex)}: " + Fore.YELLOW + f"{ex}" + Fore.WHITE)
         exit(1)
