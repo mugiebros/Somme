2d94ced1262b63718b073794f56edf5bee0f302f
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-11-24 04:22:51 -0500

    Fin pyhelp

--- pyhelp.py
+++ pyhelp.py
@@ -8,8 +8,9 @@ Par austin brodeur
 import sys
 import colorama
 from colorama import Fore
+import re
 colorama.init()
-
+GLOBALS = {}
 def main() -> None:
     """
         programme principale
@@ -18,6 +19,25 @@ def main() -> None:
         print(Fore.YELLOW + "[AB] " + Fore.RED + f"Le code s'attend a recevoir 1 argument, mais vous en avez fourni {len(sys.argv[1:])}",
               file=sys.stderr)
         print(Fore.YELLOW + "Usage: ./pyhelp.py sujet" + Fore.WHITE, file=sys.stderr)
+        sys.exit(1)
+
+    if sys.argv[1].count('.') == 1:
+        pattern = re.compile("^[^.][.A-Za-z0-9_-]*$")
+    else:
+        pattern = re.compile("^[^.]([A-Za-z0-9_-]|[^.][.][^.])*[^.]$")
+
+    if pattern.match(sys.argv[1]):
+        print(Fore.YELLOW + "[AB] " + Fore.WHITE + "Affichage de l'aide pour: " + Fore.MAGENTA + f"{sys.argv[1]}" + Fore.GREEN,file=sys.stderr)
+        try:
+            exec(f""+sys.argv[1]+"()",GLOBALS)
+        except Exception as exep:
+            if exep.__str__() == f"name '{sys.argv[1]}' is not defined":
+                print(Fore.YELLOW + "[AB] " + Fore.RED + f"{exep}")
+            else:
+                help(sys.argv[1])
+    else:
+        print(Fore.YELLOW + "[AB] " + Fore.RED + "Le sujet de l'aide n'est pas valide: " + Fore.MAGENTA + f"{sys.argv[1]}",
+              file=sys.stderr)
         sys.exit(1)
 
 if __name__ == '__main__':
