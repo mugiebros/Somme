9cb8bbfba4dfba6a27560a7056e86aa442b511ad
mugiebros <72838745+mugiebros@users.noreply.github.com>
2020-12-09 20:50:07 -0500

    fin ipc4

--- ipc5_fichier.py
+++ ipc5_fichier.py
@@ -6,10 +6,12 @@
 Par austin brodeur
 """
 import sys
+import ctypes
 from m_safe_eval import safe_eval
-from multiprocessing import Process
+from multiprocessing import Process, Value
 from typing import NoReturn
 import colorama
+import pickle
 from colorama import Fore
 colorama.init()
 DELAI_SEC = 2.0
@@ -22,15 +24,16 @@ def exexit(ex: BaseException, exit_code: int = 1) -> NoReturn:
           file=sys.stderr, sep='')
     sys.exit(exit_code)
 
-def pyval(expr: str, retour: list) -> None:
+def pyval(expr: str, filename: str) -> None:
     """
     évalue une expression
     retour via argument
     """
     try:
-        assert retour[0] == 99
-        retour[0] = safe_eval(expr)
-        print(expr, '=', retour[0])
+        évaluation = safe_eval(expr)
+        print(expr, '=', évaluation)
+        with open (filename, 'w+b') as f:
+            pickle.dump(évaluation, f)
 
     except BaseException as ex:
         exexit(ex)
@@ -40,15 +43,17 @@ def main() -> None:
     """fonction principale"""
     try:
         expr = ' '.join(sys.argv[1:]) or None
-        retour = [99]
-        ps = Process(target=pyval, args=(expr,retour))
+        filename = "ipc5.bin"
+        ps = Process(target=pyval, args=(expr,filename))
         ps.start()
         ps.join(DELAI_SEC)
         if ps.is_alive():
             ps.terminate()
             raise TimeoutError(f"Le délai de {DELAI_SEC} secondes est écoulé")
         if not ps.exitcode:
-            print(Fore.CYAN + "Arg Austin Brodeur:",Fore.RESET,retour[0])
+            with open(filename, "r+b") as f:
+                évaluation = pickle.load(f)
+            print(Fore.CYAN + "Fichier selon Austin Brodeur:",Fore.RESET,évaluation)
     except Exception as ex:
         exexit(ex)
     except KeyboardInterrupt:
